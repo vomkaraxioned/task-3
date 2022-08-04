b66c2dc099ac62b49b86ee658e47a3e4d8f36269
vomkaraxioned <omkarv@axioned.com>
2022-08-04 17:48:24 +0530

    final js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,18 @@
 /* Author: 
 
 */
+const buttons = document.getElementsByClassName('sound');
+for (x in buttons) {
+    buttons[x].addEventListener("click", listener);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function listener() {
+    let i;
+    let length = buttons.length;
+    for (i = 0; i < length; i++) {
+        buttons[i].childNodes[1].pause();
+        buttons[i].style.color = "#000"
+    }
+    this.childNodes[1].play();
+    this.style.color = "#00f";
+}
