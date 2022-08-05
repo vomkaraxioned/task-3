a8fe0eae0b7062d325b61c8fe36081c5a1451807
vomkaraxioned <omkarv@axioned.com>
2022-08-05 17:13:47 +0530

    js upadated

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,11 @@ function listener() {
     let i;
     let length = buttons.length;
     for (i = 0; i < length; i++) {
+        alert(i);
         buttons[i].childNodes[1].pause();
         buttons[i].style.color = "#fff";
     }
+    alert(this);
     this.childNodes[1].play();
     this.style.color = "#f00";
 }
