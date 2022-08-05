483d631f802a14053282f711233593739c2bcd75
vomkaraxioned <omkarv@axioned.com>
2022-08-05 17:31:40 +0530

    js change

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,9 @@ function listener() {
     let i;
     let length = buttons.length;
     for (i = 0; i < length; i++) {
-        alert(i);
         buttons[i].childNodes[1].pause();
         buttons[i].style.color = "#fff";
     }
-    alert(this);
     this.childNodes[1].play();
     this.style.color = "#f00";
 }
